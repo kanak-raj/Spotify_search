4356f5c3c720046818ea6db08c1a05987720e7c7
KANAK RAJ <72430136+kanak-raj@users.noreply.github.com>
2022-06-10 16:49:32 +0530

    1st commit

---
+++ search.py
@@ -0,0 +1,12 @@
+print("***** Welcome to Spotify search. Search for an artist and get their top tracks! *****")
+name=input("Enter artist name: ")
+#get the artist from name
+print("These are the top 3 search results: ")
+# a loop for displaying names
+print("\n")
+choice=input("Pick an artist number to get their top 3 tracks: ")
+#get the tracks on basis of choice
+print("These are the top 3 tracks of ",name,":")
+#get the tracks 
+print("\n")
+print("Hope you enjoy the tracks!")
